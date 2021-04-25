b9c44b0a3b9fa4a229f28119dde74dd19b4a9f45
Kalle Orelma <63598919+KakkuCake@users.noreply.github.com>
2021-04-25 20:00:36 +0300

    Update mygrep.c

--- unix-utilities/grep/mygrep.c
+++ unix-utilities/grep/mygrep.c
@@ -6,27 +6,38 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
-    FILE *fp;
-    char fline[100];
 
-    if (argc < 3) {
-      printf("Not enough command line parameters given!\n");
-      exit(1);
-    } 
+    	FILE *fp;
+    	char fline[100];
+    	char *line = NULL;
+  	size_t len = 0;
 
-    for(int i = 2; i < argc; i++){
+	if (argc < 2) {
+      		printf("my-grep: searchterm [file ...]\n");
+      		exit(1);
+      		
+    	} else if (argc == 2) {
+    		printf("Write here the text u want to put into my-grep:\n");
+    		while ((getline(&line, &len, stdin)) != -1) {
+  			if (strstr(line, argv[1]) != NULL) {
+  				printf("Match: %s", line);
+				free(line);
+			}	
+  		}	
+    	} else {
 
-
-        if ((fp = fopen(argv[i], "r")) == NULL) {
-            printf("File could not be opened, found or whatever, errno is %d\n",errno);
-            exit(1);
-        } 
-
-        while (fgets(fline, 100, fp) != NULL) {
-            if (strstr(fline, argv[1]) != NULL) {
-                printf("%s", fline);
-            }
-        }
-    }
+    		for(int i = 2; i < argc; i++){
+    
+        		if ((fp = fopen(argv[i], "r")) == NULL) {
+            			printf("File %s could not be opened, found or whatever, errno is %d\n", argv[i], errno);
+         			exit(1);
+        		} 
+        		while (fgets(fline, 100, fp) != NULL) {
+            			if (strstr(fline, argv[1]) != NULL) {
+                		printf("%s", fline);
+            			}
+        		}
+   		}
+   	}
     return 0;
 }
